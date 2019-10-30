88dc2c3e6e519e8acd7c7254b381ed7d4b6fbb38
alam1386 <alam1386@gmail.com>
2019-10-30 13:01:34 -0400

    The handleItem and add Item functions are created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ListItems from './ListItems'
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  // handle function
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  // Add Item function 
+   addItem(e){
+     e.preventDefault(); // this function will prevent default behavior of button which you click on it and it will referesh the page.
+    const newItem = this.state.currentItem;
+    // console.log(" The values is: ", newItem);
+    if(newItem.text !==""){
+      const newItems = [...this.state.items, newItems];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
+   }
   render(){
     return(
       <div className='App'>
         <header>
-        <form id="to-do-list">
-          <input type='text' placeholder='Enter the text'/>
+        <form id="to-do-list"onSubmit={this.addItem}>
+          <input type='text' placeholder='Enter the text'
+          value={this.state.currentItem.text}
+          onChange={this.handleInput}/>
           <button type='submit'> Add</button>
         </form>
       </header>
+      <ListItems/>
       </div>
     )
   }
---
+++ src/ListItems.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './ListItems.css'
+
+function ListItems(props){
+  return(
+    <h1>List Itmes</h1>
+  )
+}
+export default ListItems
