956f7b67d11681574b60f276e59aa476c8950d25
alam1386 <alam1386@gmail.com>
2019-11-04 12:27:54 -0500

    Updates

---
+++ precache-manifest.2eb0f75f55a382356f0938177cdcd0bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f55b5e2761f6a521a418de507100c9",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "fc3b8e28e4eb93072be9",
+    "url": "/todolist/static/css/main.c1a83295.chunk.css"
+  },
+  {
+    "revision": "064a8906d77e581939d8",
+    "url": "/todolist/static/js/2.5cd5c28a.chunk.js"
+  },
+  {
+    "revision": "fc3b8e28e4eb93072be9",
+    "url": "/todolist/static/js/main.2ab61727.chunk.js"
+  },
+  {
+    "revision": "670214c0265e7a36915f",
+    "url": "/todolist/static/js/runtime-main.026291c4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/todolist/static/media/logo.25bf045c.svg"
+  }
+]);
